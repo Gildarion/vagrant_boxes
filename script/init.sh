2dce0c0416ad78599ed39715b296c4a51147082d
Juan Miguel Martin Cabello <jmarcab01@gmail.com>
2019-09-05 20:20:04 +0200

    changing init script

--- script/init.sh
+++ script/init.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 #Exit immediately if a command exits with a non-zero status
 set -e
 #Print commands and their arguments as they are executed
 set -x
 
-sudo yum -y update
-sudo yum -y install epel-release
+yum -y update
+yum -y install epel-release
+
+#Install guest additions 6.0.10
+yum -y install perl gcc dkms kernel-devel-$(uname -r) kernel-headers-$(uname -r) make bzip2
+curl https://download.virtualbox.org/virtualbox/6.0.10/VBoxGuestAdditions_6.0.10.iso \
+-o /tmp/vbox.iso
+mkdir /tmp/VBoxGuestAdditions
+mount -o loop,ro /tmp/vbox.iso /tmp/VBoxGuestAdditions
+sh /tmp/VBoxGuestAdditions/VBoxLinuxAdditions.run
+umount /tmp/VBoxGuestAdditions
+rm -rf /tmp/*
